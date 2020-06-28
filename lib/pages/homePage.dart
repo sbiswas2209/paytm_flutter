9ea05e5580fc202e966409aa8fe0d3a98de656e1
sbiswas2209 <sbiswas2209@outlook.com>
2020-06-28 11:05:39 +0530

    Changed Home Page

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -8,34 +8,104 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String amount;
+  List title = ['Potato',
+                'Onion',
+                'Beans'];
+  List images = ['assets/images/potato.jpg',
+                'assets/images/onion.jpg',
+                'assets/images/beans.jpg'];
+  List price = [30.0,40.0,50.0];
+  List count = [0 , 0 , 0];
+  double _amount = 0.0;
+  _calcTotal(){
+    double temp = 0;
+    for(int i = 0 ; i < title.length ; i++){
+      temp += price[i]*count[i];
+    }
+    setState(() {
+      _amount = temp;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Paytm Sample'),
-      ),
-      body: ListView(
-        children: <Widget>[
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              hintText: 'Amount',
-            ),
-            onChanged: (value) {
-              setState(() {
-                amount = value;
-              });
-            },
-          ),
-          RaisedButton(
-            child: Text('Pay'),
-            onPressed: (){
-              if(amount != null){
-                Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new PaymentScreen(amount: this.amount)));
-              }
-            },
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.payment),
+            label: Text('$_amount'),
+            onPressed: () => Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new PaymentScreen(amount : _amount.toString()))),
           ),
         ],
+      ),
+      body: CustomScrollView(
+          slivers: <Widget>[
+            SliverGrid(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200.0,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
+          ),
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context , int index){
+              return Container(
+                child: Stack(
+                  children: <Widget>[
+                    Image.asset(
+                      images[index],
+                      fit: BoxFit.fill,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text('${title[index]}',
+                          style: TextStyle(
+                            backgroundColor: Colors.white,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            FlatButton(
+                              onPressed: (){
+                                if(count[index] > 0){
+                                  setState(() {
+                                    count[index] = count[index] - 1;
+                                  });
+                                  _calcTotal();
+                                }
+                              },
+                              child: Icon(Icons.remove_circle_outline),
+                            ),
+                            Text('${count[index]}kg',
+                              style: TextStyle(
+                                color: Colors.black,
+                                backgroundColor: Colors.white,
+                              ),
+                            ),
+                            FlatButton(
+                              onPressed: (){
+                                setState(() {
+                                  count[index] = count[index] + 1;
+                                });
+                                _calcTotal();
+                              },
+                              child: Icon(Icons.add_circle_outline),
+                            )
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            },
+            childCount: 3,
+          ),
+        ),
+          ],
       ),
     );
   }
